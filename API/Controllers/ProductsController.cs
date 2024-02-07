4bc78bd6e06170b7ae875c781a430d0f7bd345fc
Mustafa Ulaş <mustafaulas@windowslive.com>
2024-02-07 12:02:17 +0300

    IMemoryCache has been added on  API side.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using API.Dto;
 using Business;
 using Core.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace API.Controllers
 {
@@ -13,16 +14,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly IMemoryCache _memoryCache;
         private readonly INebimIntegrationService _nebimIntegrationService;
-        public ProductsController(INebimIntegrationService nebimIntegrationService)
+        private readonly string cacheKey="product_list";
+
+        public ProductsController(INebimIntegrationService nebimIntegrationService,IMemoryCache memoryCache)
         {
             _nebimIntegrationService=nebimIntegrationService;
+            _memoryCache=memoryCache;
         }
 
 
         [HttpGet]
         public IActionResult GetAllProducts()
         {
+        
+            List<GetB2BProductsResponse> productList=new List<GetB2BProductsResponse>();
             var nebimParameter = new ProcedureRequest
             {
                 ProcName = "usp_B2B_Products",
@@ -31,7 +38,18 @@ namespace API.Controllers
 
                 }
             };
-            var productList = _nebimIntegrationService.RunNebimProc<List<GetB2BProductsResponse>>(nebimParameter);
+        
+            if (!_memoryCache.TryGetValue(cacheKey, out productList))
+            {
+               productList= _nebimIntegrationService.RunNebimProc<List<GetB2BProductsResponse>>(nebimParameter);
+
+
+                var cacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(12)).SetPriority(CacheItemPriority.Normal)
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(12));
+
+                _memoryCache.Set(cacheKey, productList, cacheOptions);
+            }
+      
 
             if (productList.Any())
             return Ok(productList);
--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 IConfiguration configuration = builder.Configuration;
 builder.Services.AddControllers();
+builder.Services.AddMemoryCache();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

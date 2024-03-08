5d37e1f495e7a4a8ae10a791ff08975ff9b5483a
Mustafa Ulaş <mustafaulas@windowslive.com>
2024-03-08 15:24:11 +0300

    First init of repo

--- client/src/app/shared/models/productListResponse.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from "./products";
-
-export interface ProductListResponse {
-    data:Product[],
-    pageNumber:number,
-    pageSize:number,
-    totalCount:number
-}
--- client/src/app/shared/models/products.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    productCode:string,
-    productName:string,
-    picturePath:string,
-    gender:string,
-    mainGroup:string,
-    subGroup:string,
-    model:string
-   }
